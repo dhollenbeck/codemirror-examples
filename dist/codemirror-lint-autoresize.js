fa4bed8d97103a64429887ac6b5b90846c39eeb0
Daniel Hollenbeck <dan@hollenbecks.com>
2018-01-23 09:18:07 -0600

    back to single returning single editor

--- dist/codemirror-lint-autoresize.js
+++ dist/codemirror-lint-autoresize.js
@@ -30281,50 +30281,43 @@ var ruleSets = {
 };
 
 $.fn.editor = function(options) {
-		
+	
+	var textarea, mode, editor;
+
     options = options || {};
     options.height = options.height || 'auto';
+   
+    textarea = $(this);
+    mode = options.mode || textarea.attr('mode') || 'html';
+    if (mode === 'html') mode = 'text/html';
+    if (mode === 'handlebars') mode = {name: "handlebars", base: "text/html"};
 
-    var editors = [];
+    editor = CodeMirror.fromTextArea(textarea.get(0), {
+        mode: mode,
+        readOnly: options.readOnly,
+        indentUnit: 4,
+        styleActiveLine: true,
+        lineNumbers: true,
+        lineWrapping: true,
+        indentWithTabs: true,
+        autoCloseTags: true,
+        foldGutter: true,
+        dragDrop: true,
+        lint: true,
+        viewportMargin: Infinity,
+        gutters: ['CodeMirror-lint-markers', 'CodeMirror-linenumbers', 'CodeMirror-foldgutter']
+    });
+    editor.setSize(null, options.height);
 
-    this.each(function() {
+    editor.on('change', function(editor) {
+        editor.save();
 
-        var textarea, mode, editor;
+        // validate with https://github.com/provejs/provejs-jquery
+        if (textarea.dirty) textarea.dirty(true);
+        if (textarea.validate) textarea.validate();
 
-        textarea = $(this);
-        mode = options.mode || textarea.attr('mode') || 'html';
-        if (mode === 'html') mode = 'text/html';
-        if (mode === 'handlebars') mode = {name: "handlebars", base: "text/html"};
-
-        editor = CodeMirror.fromTextArea(textarea.get(0), {
-            mode: mode,
-            readOnly: options.readOnly,
-            indentUnit: 4,
-            styleActiveLine: true,
-            lineNumbers: true,
-            lineWrapping: true,
-            indentWithTabs: true,
-            autoCloseTags: true,
-            foldGutter: true,
-            dragDrop: true,
-            lint: true,
-            viewportMargin: Infinity,
-            gutters: ['CodeMirror-lint-markers', 'CodeMirror-linenumbers', 'CodeMirror-foldgutter']
-        });
-        editor.setSize(null, options.height);
-
-        editor.on('change', function(editor) {
-            editor.save();
-
-            // validate with https://github.com/provejs/provejs-jquery
-            if (textarea.dirty) textarea.dirty(true);
-            if (textarea.validate) textarea.validate();
-
-            textarea.trigger('editor.change', editor);
-        });
-
-        editors.push(editor);
+        textarea.trigger('editor.change', editor);
     });
 
-    return this;
+    return editor;
 };
