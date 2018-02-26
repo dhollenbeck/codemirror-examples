6728ad9946c323868adbecb729f39f955d3c3b00
dhollenbeck <dan@hollenbecks.com>
2018-02-26 08:12:07 -0600

    cleanup

--- grunts/concat.js
+++ grunts/concat.js
@@ -12,49 +12,6 @@ module.exports = function(grunt) {
 				return src;
 			}
 		},
-		// js: {
-		// 	nonull: true,
-		// 	dest: 'dist/codemirror-examples.js',
-		// 	src: [
-        //         'libs/codemirror/5.27.4/codemirror.js',
-        //         'libs/codemirror/5.27.4/mode/javascript/javascript.js',
-        //         'libs/codemirror/5.27.4/mode/xml/xml.js',
-        //         'libs/codemirror/5.27.4/mode/css/css.js',
-        //         'libs/codemirror/5.27.4/addon/mode/simple.js',
-        //         'libs/codemirror/5.27.4/addon/mode/multiplex.js',
-        //         'libs/codemirror/5.27.4/mode/handlebars/handlebars.js',
-        //         'libs/codemirror/5.27.4/addon/fold/comment-fold.js',
-        //         'libs/codemirror/5.27.4/addon/search/search.js',
-        //         'libs/codemirror/5.27.4/addon/search/searchcursor.js',
-        //         'libs/codemirror/5.27.4/addon/dialog/dialog.js',
-        //         'libs/codemirror/5.27.4/addon/hint/show-hint.js',
-        //         'libs/codemirror/5.27.4/addon/hint/xml-hint.js',
-        //         'libs/codemirror/5.27.4/addon/hint/html-hint.js',
-        //         'libs/codemirror/5.27.4/addon/hint/css-hint.js',
-        //         'libs/codemirror/5.27.4/addon/hint/javascript-hint.js',
-        //         'libs/codemirror/5.27.4/addon/search/match-highlighter.js',
-        //         'libs/codemirror/5.27.4/addon/lint/lint.js',
-        //         'libs/codemirror/5.27.4/addon/lint/html-lint.js',
-        //         'libs/codemirror/5.27.4/addon/lint/css-lint.js',
-        //         'libs/codemirror/5.27.4/addon/lint/javascript-lint.js',
-        //         'libs/codemirror/5.27.4/addon/search/jump-to-line.js',
-        //         'libs/codemirror/5.27.4/mode/markdown/markdown.js',
-        //         'libs/codemirror/5.27.4/linting/handlebars.js',
-
-        //         // 3rd party libs
-        //         'libs/htmlhint/htmlhint.js',
-        //         'libs/jshint/jshint.js',
-                
-        //         // handlebars linting
-        //         './node_modules/handlebars/dist/handlebars.min.js',
-        //         './node_modules/handlebars-error-parser/index.js',
-
-        //         // Warning: csslint.js must be loaded after Handlebars 
-        //         // otherwise it removes `window.Handlebars`.
-        //         'libs/csslint/csslint.js',
-        //         'libs/editor.js'
-		// 	]
-        // },
         js: {
 			nonull: true,
 			dest: 'dist/codemirror-examples.js',
@@ -83,20 +40,18 @@ module.exports = function(grunt) {
                 './node_modules/codemirror/addon/search/jump-to-line.js',
                 './node_modules/codemirror/mode/markdown/markdown.js',
 
-                'libs/linting/handlebars.js',
+                'libs/addon/lint/handlebars-lint.js',
 
                 // 3rd party libs
                 './node_modules/htmlhint/lib/htmlhint.js',
-                'libs/jshint/jshint.js',
-                
-                // handlebars linting
+                './node_modules/jshint/dist/jshint.js',
                 './node_modules/handlebars/dist/handlebars.min.js',
                 './node_modules/handlebars-error-parser/index.js',
 
                 // Warning: csslint.js must be loaded after Handlebars 
                 // otherwise it removes `window.Handlebars`.
                 './node_modules/csslint/dist/csslint.js',
-                'libs/editor.js'
+                'libs/jquery/editor.js'
 			]
         },
 		css: {
