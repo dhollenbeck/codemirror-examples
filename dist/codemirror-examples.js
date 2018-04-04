ef96061b2962a0c83c302d8ecaef81655dc68c49
dhollenbeck <dan@hollenbecks.com>
2018-04-04 08:37:09 -0500

    more linter work

--- dist/codemirror-examples.js
+++ dist/codemirror-examples.js
@@ -48201,6 +48201,8 @@ function getPos(lines, lineNum, code, indicator) {
 	var line, min, max, dots = false, prefix = 0;
 	var pos = {};
 
+	// console.log('code', code);
+
 	// handle special cases
 	if (code === '{{{{') {
 		pos.min = 0;
@@ -48251,7 +48253,7 @@ function getPos(lines, lineNum, code, indicator) {
 	if (min === -1) {
 		return {
 			min: 0,
-			max: 0
+			max: 1
 		};
 	} else {
 		return {
@@ -48277,7 +48279,7 @@ exports.parser = function (e, html) {
 		lineNum = lineNum - 1;
 		pos = getPos(lines, lineNum, code, indicator);
 
-		//console.log('pos:', pos);
+		// console.log('pos:', pos);
 
 		parsed.start = {
 			line: lineNum,
@@ -48476,7 +48478,7 @@ function lintHelperParam(astHelper, rule, ruleKey) {
 	} else if (isMissingParams) {
 		return {
 			severity: rule.severity,
-			message: Messages.get('param', rule, params),
+			message: Messages.get('param-missing', rule, params),
 			start: {
 				line: astHelper.loc.start.line - 1,
 				column: astHelper.loc.start.column
@@ -48579,7 +48581,7 @@ exports.configs = {
 	each: {
 		block: true,
 		params: {
-			arrValue: {
+			'array': {
 				selector: 'positional(0)',
 				required: 1
 			},
@@ -48626,11 +48628,8 @@ exports.configs = {
 },{"./formats":55,"./messages":57,"./selectors":58,"./walker":59,"lodash.forown":37,"lodash.isfunction":39,"lodash.isobject":40,"lodash.keys":42}],57:[function(require,module,exports){
 'use strict';
 
-
-function words(val) {
-	if (val === false) return 'an optional';
-	if (val === true) return 'one';
-	if (val === 0) return 'an optional';
+function word(val) {
+	if (val === 0) return 'zero';
 	if (val === 1) return 'one';
 	if (val === 2) return 'two';
 	if (val === 3) return 'three';
@@ -48651,12 +48650,21 @@ function errorBlock(rule) {
 	return message;
 }
 
-function errorParams(rule, params) {
-	console.log('errorParams()');
-	var message = (rule.required === true)
-		? exports.format('The {{@helper.name}} helper requires ' + words(rule.required) + ' `@rule.name` params, but only ' + params.length + ' were found.', rule)
-		: exports.format('The {{@helper.name}} helper requires a `@rule.name` parameter, but non was found.', rule);
+function errorParamMissing(rule, params) {
+	console.log('errorParamMissing()');
+	var message;
+	if (rule.required === true && params.length === 0) {
+		message = exports.format('The {{@helper.name}} helper requires a `@rule.name` parameter, which was not found.', rule);
+	} else if (rule.required === 1) {
+		message = exports.format('The {{@helper.name}} helper requires ' + word(rule.required) + ' `@rule.name` parameter, but which was not.', rule);
+	} else {
+		message = exports.format('The {{@helper.name}} helper requires ' + word(rule.required) + ' `@rule.name` parameters, but ' + word(params.length) + ' were found.', rule);
+	}
 	return message;
+}
+
+function near(code) {
+	return 'near `' + code.trim() + '`';
 }
 
 exports.parser = function (str, code) {
@@ -48665,34 +48673,34 @@ exports.parser = function (str, code) {
 	// console.log('* code:', code);
 
 	if (str.indexOf("got 'INVALID'") !== -1)
-		str = 'Invalid or incomplete Handlebars expression.';
+		str = 'Invalid or incomplete Handlebars expression ' + near(code) + '.';
 
 	if (str === "Expecting 'ID', 'STRING', 'NUMBER', 'BOOLEAN', 'UNDEFINED', 'NULL', 'DATA', got 'EOF'")
-		str = 'Empty or incomplete Handlebars expression.';
+		str = 'Empty or incomplete Handlebars expression ' + near(code) + '.';
 
 	if (str === "Expecting 'EOF', got 'OPEN_ENDBLOCK'")
-		str = 'Invalid closing block, check opening block.';
+		str = 'Invalid closing block, check opening block ' + near(code) + '.';
 
 	if (str === "Expecting 'ID', 'STRING', 'NUMBER', 'BOOLEAN', 'UNDEFINED', 'NULL', 'DATA', got 'CLOSE'")
-		str = 'Empty expression.';
+		str = 'Empty expression ' + near(code) + '.';
 
 	if (str === "Expecting 'ID', 'STRING', 'NUMBER', 'BOOLEAN', 'UNDEFINED', 'NULL', 'DATA', got 'CLOSE_UNESCAPED'")
-		str = 'Empty expression.';
+		str = 'Empty expression ' + near(code) + '.';
 
 	if (str === "Expecting 'CLOSE_RAW_BLOCK', 'CLOSE', 'CLOSE_UNESCAPED', 'OPEN_SEXPR', 'CLOSE_SEXPR', 'ID', 'OPEN_BLOCK_PARAMS', 'STRING', 'NUMBER', 'BOOLEAN', 'UNDEFINED', 'NULL', 'DATA', 'SEP', got 'OPEN'")
-		str = 'Invalid expression.';
+		str = 'Invalid expression ' + near(code) + '.';
 
 	if (str === "Expecting 'CLOSE', 'OPEN_SEXPR', 'ID', 'STRING', 'NUMBER', 'BOOLEAN', 'UNDEFINED', 'NULL', 'DATA', got 'CLOSE_RAW_BLOCK'")
-		str = 'Invalid expression.';
+		str = 'Invalid expression ' + near(code) + '.';
 
 	if (str.indexOf("Expecting 'OPEN_INVERSE_CHAIN', 'INVERSE', 'OPEN_ENDBLOCK', got 'EOF'") !== -1)
-		str = 'Missing block closing expression for code near ' + code + '.';
+		str = 'Missing block closing expression ' + near(code) + '.';
 
 	if (str.indexOf("', got 'EOF'") !== -1)
-		str = 'Missing closing expression for code near ' + code + '.';
+		str = 'Missing closing expression ' + near(code) + '.';
 
 	if (str.indexOf("', got '") !== -1)
-		str = 'Invalid expression.';
+		str = 'Invalid expression ' + near(code) + '.';
 
 	if (str.indexOf("doesn't match") !== -1)
 		str = 'The opening and closing expressions do not match. Specifically, ' + mismatch(str) + '.';
@@ -48709,7 +48717,7 @@ function mismatch(str) {
 exports.get = function(type, rule, params) {
 	if (type === 'block') return errorBlock(rule);
 	if (type === 'formats') return errorFormats(rule);
-	if (type === 'param') return errorParams(rule, params);
+	if (type === 'param-missing') return errorParamMissing(rule, params);
 };
 
 exports.format = function(message, rule) {
